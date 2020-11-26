be6c5d37897323402e6a617057f5865266b924ad
Oleg Andrianov <43012841+pow44feet@users.noreply.github.com>
2020-11-26 20:53:08 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ document.body.classList.add('loaded_hiding');
 window.setTimeout(function () {
 document.body.classList.add('loaded');
 document.body.classList.remove('loaded_hiding');
-}, 3000);
+}, 200);
 }
 
 //Default function
